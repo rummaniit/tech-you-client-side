3956a1d40e412b847081a74bee5b077e46dce719
rummaniit <rummanoffice12@gmail.com>
2022-10-25 18:25:59 +0600

    header and other installations

---
+++ src/Pages/Routes/Route.js
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Root/Main/Main";
+import Blog from "../Blog/Blog";
+import Courses from "../Courses/Courses";
+import Faq from "../Faq/Faq";
+import Home from "../Home/Home";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: ([
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ])
+    }
+])
